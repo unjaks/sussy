77cd0081ddf35f34ebd9ff386e1b22a2ffa8c038
unjak <144765110+unjaks@users.noreply.github.com>
2024-10-29 01:59:49 +0700

    Update highlight.lua

--- highlight.lua
+++ highlight.lua
@@ -6,9 +6,9 @@ local settings_tbl = {
     ESP_Enabled = true,
     ESP_TeamCheck = false,
     Chams = true,
-    Chams_Color = Color3.fromRGB(255, 255, 255),
+    Chams_Color = Color3.fromRGB(0, 0, 0),
     Chams_Transparency = 0,
-    Chams_Glow_Color = Color3.fromRGB(255, 0, 0)
+    Chams_Glow_Color = Color3.fromRGB(255, 255, 255)
 }
 
 function destroy_chams(char)
