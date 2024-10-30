8b13040aaee1e1edf6a1f4144dd587828ec9c3f3
unjak <144765110+unjaks@users.noreply.github.com>
2024-10-30 20:12:54 +0700

    Update highlight.lua

--- highlight.lua
+++ highlight.lua
@@ -8,7 +8,7 @@ local settings_tbl = {
     Chams = true,
     Chams_Color = Color3.fromRGB(0, 0, 0),
     Chams_Transparency = 0.5,
-    Chams_Glow_Color = Color3.fromRGB(255, 255, 255)
+    Chams_Glow_Color = Color3.fromRGB(255, 255, 255),
     Chams_Glow_Transparency = 0.5
 }
 
