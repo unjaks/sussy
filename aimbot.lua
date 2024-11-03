fb9abeacf1c67dbd5829297ce612572c41446871
unjak <144765110+unjaks@users.noreply.github.com>
2024-11-03 14:39:13 +0700

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -1,9 +1,9 @@
-local teamCheck = true
+local teamCheck = false
 local fov = 40
 local smoothing = 0.09
 local predictionFactor = 0.1  -- Adjust this factor to improve prediction accuracy
 local highlightEnabled = true  -- Variable to enable or disable target highlighting. Change to False if using an ESP script.
-local lockPart = "Torso"  -- Choose what part it locks onto. Ex. HumanoidRootPart or Head
+local lockPart = "Head"  -- Choose what part it locks onto. Ex. HumanoidRootPart or Head
 
 local Toggle = false  -- Enable or disable toggle mode
 local ToggleKey = Enum.KeyCode.E  -- Choose the key for toggling aimbot lock
